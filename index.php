3a18df571ff6a9e2575e3ddfb2c4548b9f3dcb56
alturismo <mabenamara@freenet.de>
2020-11-01 16:24:09 +0000

    updates / fixes

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 
 // fetch args from link local or remote
 if ($_GET) {
-    $argument1 = $_GET['streamtype'];
-    $argument2 = $_GET['streamlink'];
+    $streamtype = $_GET['streamtype'];
+    $streamlink = $_GET['streamlink'];
 } else {
-    $argument1 = $argv[1];
-    $argument2 = $argv[2];
+    $streamtype = $argv[1];
+    $streamlink = $argv[2];
 }
 
 // setup inputstream
-$inputline = "http://localhost:34400/stream/$argument2";	// format inputline
+$inputline = "http://localhost:34400/stream/$streamlink";	// format inputline
 //
 $bitratelow = "400k";	// lowwest bitrate
 $bitratemed = "750k";	// medium  bitrate
@@ -22,11 +22,21 @@ $resolution = "854";	// resolution width, Aspect ratio is kept
 // end config here
 //
 
-// set streamfile
-$streamdata = 'master.m3u8';
+// set streamfile and continue
+unset ($streamdata);
+
+if ($streamtype == "hls") {
+	$streamdata = 'master.m3u8';
+}
+if ($streamtype == "dash") {
+	$streamdata = 'dash.mpd';
+}
+if (empty($streamdata)) {
+	exit ();
+}
 
 // set controlfile if stream is already up and running
-$controlfile = "$argument1.$argument2.streamactive";
+$controlfile = "$streamtype.$streamlink.streamactive";
 
 // check if stream already running
 if (file_exists($controlfile)) {
@@ -49,49 +59,57 @@ exit ();
 
 } else {
 
-// remove old controlfile to clear running hls stream
+// remove old controlfile to clear running stream
 unset ($oldcontrolfile);
-$oldcontrolfile = glob('hls.*.streamactive');
+$oldcontrolfile = glob('*.streamactive');
 if (!empty($oldcontrolfile)) {
-	exec ("rm hls.*.streamactive");
+	exec ("rm *.streamactive");
 }
 
 // write new control file
 exec ("echo > $controlfile");
 
-// remove manifest to process only on new one
-if (file_exists($streamdata)) {
-	unlink ($streamdata);
-}
-
 // end old ffmpeg stream
-$ffmpegtype = shell_exec ("pgrep -fl $argument1");
+$ffmpegactive = shell_exec ("pgrep -fl $resolution");
 
-if (fnmatch("*ffmpeg*", $ffmpegtype)) {
-	$ffmpegtype = strtok($ffmpegtype, " ");
+if (fnmatch("*ffmpeg*", $ffmpegactive)) {
+	$ffmpegactive = strtok($ffmpegactive, " ");
 } else {
-	unset ($ffmpegtype);
+	unset ($ffmpegactive);
 }
 
-if (!empty($ffmpegtype)) {
-	exec ("kill $ffmpegtype");
-	unset ($ffmpegtype);
+if (!empty($ffmpegactive)) {
+	exec ("kill $ffmpegactive");
+	unset ($ffmpegactive);
 }
+
+// remove manifest and temp files to process only on new one
+shell_exec ("rm master.m3u8");
+shell_exec ("rm dash.mpd");
+shell_exec ("rm -R v*/*");
 
 // run ffmpeg with arguments and settings
 $cmd1 = escapeshellcmd ('ffmpeg -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -hwaccel_output_format vaapi -hide_banner -loglevel error -i');
 $cmd2 = escapeshellcmd ('-y -vf format=vaapi,hwupload,deinterlace_vaapi,scale_vaapi=');
-$cmd3 = escapeshellcmd (':-1 -c:v h264_vaapi -c:a aac -ar 48000 -ac 2 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -c:v:0 h264_vaapi -b:v:0');
-$cmd4 = escapeshellcmd ('-c:v:1 h264_vaapi -b:v:1');
-$cmd5 = escapeshellcmd ('-c:v:2 h264_vaapi -b:v:2');
-$cmd6 = escapeshellcmd ('-c:a:0 aac -ar 48000 -ac 2 -b:a:0 128k -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" -master_pl_name master.m3u8 -f hls -hls_time 4 -hls_list_size 10 -hls_flags delete_segments -hls_segment_filename "v%v/fileSequence%d.ts" v%v/prog_index.m3u8');
+if ($streamtype == "hls") {
+	$cmd3 = escapeshellcmd (':-1 -c:v h264_vaapi -c:a aac -ar 48000 -ac 2 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -c:v:0 h264_vaapi -b:v:0');
+	$cmd4 = escapeshellcmd ('-c:v:1 h264_vaapi -b:v:1');
+	$cmd5 = escapeshellcmd ('-c:v:2 h264_vaapi -b:v:2');
+	$cmd6 = escapeshellcmd ('-c:a:0 aac -ar 48000 -ac 2 -b:a:0 128k -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" -master_pl_name master.m3u8 -f hls -hls_time 4 -hls_list_size 10 -hls_flags delete_segments -hls_segment_filename "v%v/fileSequence%d.ts" v%v/prog_index.m3u8');
+}
+if ($streamtype == "dash") {
+	$cmd3 = escapeshellcmd (':-1 -c:v h264_vaapi -c:a aac -ar 48000 -ac 2 -map 0:0 -map 0:0 -map 0:0 -map 0:1 -b:v:0');
+	$cmd4 = escapeshellcmd ('-b:v:1');
+	$cmd5 = escapeshellcmd ('-b:v:2');
+	$cmd6 = escapeshellcmd ('-c:a:0 aac -ar 48000 -ac 2 -b:a:0 128k -init_seg_name v$RepresentationID$/init-stream$RepresentationID$-$Bandwidth$.m4s -media_seg_name v$RepresentationID$/chunk-stream$RepresentationID$-$Bandwidth$-$Number%05d$.m4s -use_template 1 -use_timeline 1 -seg_duration 4 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash dash.mpd');
+}
 
 exec ("$cmd1 $inputline $cmd2$resolution$cmd3 $bitratelow $cmd4 $bitratemed $cmd5 $bitratehigh $cmd6 >/dev/null 2>/dev/null &");
 
 // wait for Manifest or timeout 30 seconds (php.ini)
 while (!file_exists($streamdata));
 
-// relink to new HLS stream
+// relink to new stream
 if (file_exists($streamdata)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
@@ -103,8 +121,6 @@ if (file_exists($streamdata)) {
     readfile($streamdata);
     exit;
 }
-
-// exec ("nohup sh test.sh >/dev/null 2>/dev/null &");
 
 // exit here
 exit();
--- watch.sh
+++ watch.sh
@@ -1,36 +1,65 @@
 #!/bin/sh
 
 log_file="activity.log"
-log_search="fileSequence"
+hls_search="fileSequence"
+dash_search="chunk"
 
 hls_master="master.m3u8"
+dash_master="dash.mpd"
 hls_timeout=60
-hls_sleep=30
+hls_sleep=15
 
 tail -fn0 $log_file | \
 
 while read line ; do
-	echo "$line" | grep $log_search
+	echo "$line" | grep -e $hls_search -e $dash_search
 	if [ $? = 0 ]
-		then
-		hlspid=$(pgrep -f $hls_master)
-		if [ $hlspid ]
-			then
-			sleep $hls_sleep
-			age=$(expr $(date +%s) - $(stat -c %Y ./$log_file))
-			echo "found pid: $hlspid age: $age"
-			if [ $age -gt $hls_timeout ]
+		then		
+		case $line in
+			*"$hls_search"*)
+			hlspid=$(pgrep -f $hls_master)
+			if [ $hlspid ]
 				then
-				kill $hlspid
-				rm /phpserver/$hls_master
-				rm /phpserver/hls.*
-				rm /phpserver/dash.*
-				echo "killed $hlspid cause $age seconds old and removed files"
-				sleep 3
-				age=0
-				hlspid=$(pgrep -f $hls_master)
-				continue
+				sleep $hls_sleep
+				age_hls=$(expr $(date +%s) - $(stat -c %Y ./$log_file))
+				if [ $age_hls -gt $hls_timeout ]
+					then
+					kill $hlspid
+					rm /phpserver/$hls_master
+					rm /phpserver/hls.*
+					rm -R /phpserver/v*/*
+					echo "killed cause $age_hls seconds idle and removed files"
+					sleep 3
+					age_hls=0
+					hlspid=$(pgrep -f $hls_master)
+					continue
+				fi
 			fi
-		fi
+			;;
+			*)
+		esac
+		case $line in
+			*"$dash_search"*)
+			dashpid=$(pgrep -f $dash_master)
+			if [ $dashpid ]
+				then
+				sleep $hls_sleep
+				age_dash=$(expr $(date +%s) - $(stat -c %Y ./$log_file))
+				if [ $age_dash -gt $hls_timeout ]
+					then
+					kill $dashpid
+					rm /phpserver/$dash_master
+					rm /phpserver/dash.*
+					rm -R /phpserver/v*/*
+					echo "killed cause $age_dash seconds idle and removed files"
+					sleep 3
+					age_dash=0
+					dashpid=$(pgrep -f $dash_master)
+				continue
+				fi
+			fi
+			;;
+			*)
+		esac
 	fi
 done
